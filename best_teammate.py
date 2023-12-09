9d443fc67d75024a9cc5a6543cb4bedbbbcbd6a7
jrmcmill <jrmcmill@umich.edu>
2023-12-09 18:48:06 -0500

    add best teammate functionality without filtering

--- best_teammate.py
+++ best_teammate.py
@@ -11,8 +11,41 @@ STAT_TO_INDEX = {'hp': 0,
                  'special defense': 4,
                  'speed': 5}
 
-def find_best_teammate(type: str=None, stat: str=None, stat_value: int=None) -> list[str]:
-    pass
-    #get_pagerank(graph)  # need to add sorting by PageRank scores
-    # and filtering by type and stat OR base-stat total (BST)
+from pkmn_data import PokemonData
+from pkmn_tiering import PokemonTiers
+from pkmn_network import PokemonGraph
+
+
+def find_best_teammate(ranks: dict[str: float]=None, num_teammates: int=1, pokemon: str=None, teammates: list[str]=None, typing: str=None, stat: str=None, stat_value: int=None) -> None:
+    ranked = list(ranks.keys())
+
+    if typing:
+        pass
+
+    if stat:
+        pass
+
+    if not pokemon:
+        print('The best teammate(s) are:')
+        print(*ranked[:num_teammates], sep='\n')
+    
+    if pokemon:
+        print(f'The best teammate(s) for {pokemon} are:')
+        print(*[p for p in ranked if p in teammates][:num_teammates], sep='\n')
+
+if __name__ == '__main__':
+    all_data = PokemonData(BASE_URL+TEST_MONTH_URL+TEST_FORMAT_URL)
+    training_data = all_data.get_tiering_data()
+    pkmn_tiers = PokemonTiers(training_data)
+
+    tiers = pkmn_tiers.get_tiers(True)
+
+    graph = PokemonGraph()
+    edge_data = all_data.get_team_data()
+
+    graph.build_graph(edge_data, tiers)
+    pr = graph.get_pagerank(graph)
+
+    find_best_teammate(pr, 6)
+    find_best_teammate(pr, 3, 'mamoswine', all_data.get_teammates('mamoswine'))
 
