7a1446291838d24569f364416abca5db7b9d7800
jrmcmill <jrmcmill@umich.edu>
2023-12-10 15:33:49 -0500

    generate requirements.txt and add imports to user interface py

--- user_interface.py
+++ user_interface.py
@@ -2,3 +2,8 @@
 Handle user interaction with program via CLI
 '''
 
+from pkmn_data import PokemonData
+from pkmn_tiering import PokemonTiers
+from pkmn_network import PokemonGraph
+from best_teammate import find_best_teammate
+
